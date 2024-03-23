cecc127fb27e420f0c54ea0e2a097c5f6d165481
Nikita Shutov <ShutovNA1987@gmail.com>
2024-03-23 09:57:00 +0700

    Initial

---
+++ topfive-entities/src/main/java/ru/nikitos/topfive/topfive/entities/TopfiveEntitiesApplication.java
@@ -0,0 +1,13 @@
+package ru.nikitos.topfive.topfive.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopfiveEntitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopfiveEntitiesApplication.class, args);
+    }
+
+}
